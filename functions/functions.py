6a4b5cccbbffe23dcce5db59ea9e415c9f8bf2ef
Jonas McClure <jonmcclu@developernexus.app>
2020-10-30 09:53:46 -0400

    Updated with basic conversion to docx

--- functions/functions.py
+++ functions/functions.py
@@ -1,16 +1,30 @@
 import json
-import boxnotes2html
+import boxnotes2html  # https://pypi.org/project/boxnotes2html/
+from htmldocx import HtmlToDocx  # https://pypi.org/project/htmldocx/
 
 
 def import_settings():
     with open('settings.json') as settings_file:
         settings = json.load(settings_file)
-    print(settings['source_directory'])
+    return settings
 
 
 def get_filenames():
     pass
 
 
-def convert_boxnote():
-    pass
+def convert_boxnote(filename, destination):
+    note = boxnotes2html.BoxNote.from_file(filename)
+    try:
+        html_file = destination.replace('.boxnote', '.html')
+        docx_file = destination.replace('.boxnote', '')
+        html = note.as_html()
+        # print(html)
+        html_rework = html.replace('<style>body {\n    font-family: Sans-Serif;\n}\n', '')
+        with open(html_file, 'w') as file:
+            file.write(html_rework)
+
+        new_parser = HtmlToDocx()
+        new_parser.parse_html_file(html_file, docx_file)
+    except KeyError as error:
+        print(f'KeyError:  {filename}')
--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import functions.functions as functions
 import classes.classes as classes
+import os
 
 if __name__ == "__main__":
-    functions.import_settings()
+    settings = functions.import_settings()
+    print(settings['source_directory'])
+    s = settings['source_directory']
+    d = s + os.sep + 'converted'
+
+    for subdir, dirs, files in os.walk(s):
+        for filename in files:
+            filepath = subdir + os.sep + filename
+            dest_dir = subdir.replace(s, d)
+            if not os.path.isdir(dest_dir):
+                os.makedirs(dest_dir)
+
+            print(dest_dir)
+            if filepath.endswith('.boxnote'):
+                # print(filepath)
+                destination_file = filepath.replace(s, d)
+                # print(destination_file)
+                functions.convert_boxnote(filepath, destination_file)

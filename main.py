6f63e390fd39916e342b77d25fa858983f2117b6
Jonas McClure <jonmcclu@developernexus.app>
2020-11-06 07:52:50 -0500

    Created a Linux executable file for the converter script

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import functions.functions as functions
 import os
+from tkinter import filedialog
+from tkinter import *
 
 if __name__ == "__main__":
     settings = functions.import_settings()
     # print(settings['source_directory'])
     source = settings['source_directory']
 
+    root = Tk()
+    root.withdraw()
+    folder_selected = filedialog.askdirectory()
+
     for subdir, dirs, files in os.walk(source):
         for filename in files:
-            filepath = subdir + os.sep + filename
+            filepath: str = subdir + os.sep + filename
 
             if filepath.endswith('.boxnote'):
                 # print(filepath)
